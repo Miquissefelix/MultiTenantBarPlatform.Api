b4f1f9da1939beb48e35164da80bc40bf405adac
MiquisseFelix <fmiquissejose@gmail.com>
2026-01-18 21:09:16 -0800

    Add scaffolding for Application layer and validation libs
    Added empty class and interface stubs for behaviors, DTOs, commands, handlers, queries, and validators across all main features (auth, bars, cashiers, products, sales) in BarManagement.Application. Updated csproj to include FluentValidation packages for future validation logic. No business logic implemented yet.

---
+++ BarManagement/BarManagement.Application/Features/Auth/Login/LoginCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarManagement.Application.Features.Auth.Login
+{
+    internal class LoginCommand
+    {
+    }
+}
---
+++ BarManagement/BarManagement.Application/Features/Bars/Queries/GetAllBarsQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarManagement.Application.Features.Bars.Queries
+{
+    internal class GetAllBarsQuery
+    {
+    }
+}
